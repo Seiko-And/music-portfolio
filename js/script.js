8cf52a1960dd486dfae6eca4667466412ee0aa82
Seiko-And <seiko.a125@gmail.com>
2025-05-11 11:21:44 -0700

    changed some code

--- js/script.js
+++ js/script.js
@@ -1,28 +1,55 @@
 // --------------- SUB MENU TIMER ---------------------
 const subMenus = document.querySelectorAll('li.submenu');
+//  Screen size is less than 600px
+const isMobile = window.innerWidth < 600; 
+console.log(isMobile);
 
-for (let subMenu of subMenus) {
-  let timer;
-  let link = subMenu.querySelector('a');
+function menuTimer(){
 
-  subMenu.addEventListener("mouseover", function() {
-    clearTimeout(timer);
-    subMenu.classList.add("open");
-
-    if (link.parentNode.className == 'submenu open'){
-      link.setAttribute('aria-expanded', 'true');
+  for (let subMenu of subMenus) {
+    let timer;
+    let link = subMenu.querySelector('a');
+   
+    if (isMobile){
+      link.addEventListener('click', function() {
+        console.log(link);
+        console.log(subMenu);
+        const isOpen = subMenu.classList.contains('open');
+        if (isOpen){
+          link.parentNode.classList.remove('open');
+          link.setAttribute('aria-expanded', 'false');
+        }
+        else if (!isOpen){
+          link.parentNode.classList.add('open');
+          link.setAttribute('aria-expanded', 'true');
+        }
+      });
     }
-  });
-
-  subMenu.addEventListener("mouseout", function(){
-		timer = setTimeout(function(){
-			document.querySelector(".submenu.open").className = "submenu";
-      if(link.parentNode.className == 'submenu'){
-        link.setAttribute('aria-expanded', 'false');
-      }
-		}, 1500);
+    else{
+      subMenu.addEventListener("mouseover", function() {
+        clearTimeout(timer);
+        subMenu.classList.add("open");
+    
+        if (link.parentNode.className == 'submenu open'){
+          link.setAttribute('aria-expanded', 'true');
+        }
+      });
+    
+      subMenu.addEventListener("mouseout", function(){
+        timer = setTimeout(function(){
+          document.querySelector(".submenu.open").className = "submenu";
+          if(link.parentNode.className == 'submenu'){
+            link.setAttribute('aria-expanded', 'false');
+          }
+        }, 1500);
+      
+      });
+    }
   
-	});
+  };
 
-};
+}
+menuTimer();
+
+
 
